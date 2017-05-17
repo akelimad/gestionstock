e70ac0d8d5599f115604ebc2bafe8c79ca78d9eb
akel <akel.dev@gmail.com>
2017-05-17 16:17:08 +0100

    change migrate table

--- app/DoctrineMigrations/Version20170517150911.php
+++ app/DoctrineMigrations/Version20170517150911.php
@@ -8,14 +8,16 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170517142314 extends AbstractMigration
+class Version20170517150911 extends AbstractMigration
 {
     /**
      * @param Schema $schema
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('
+            INSERT INTO user (username, enabled, password) VALUES ("test", "1", "$2y$13$AXD86k1MQkM9rWBEi78f6ORkGB42AFBMS188.DU/bHTYsCN0WyFLW");
+        ');
 
     }
 
