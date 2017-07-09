b6242e9614a6919c7967b15b3a5be7b59a4b4e48
Akel <akel.dev@gmail.com>
2017-07-09 21:55:01 +0100

    fix home routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,12 +8,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/{_locale}/")
+     * @Route("/", name="app_default_index")
+     */
+    public function homeAction()
+    {
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/{_locale}", name="app_default_locale")
      */
     public function indexAction()
     {
-        
         return $this->render('AppBundle:Default:index.html.twig');
     }
 
